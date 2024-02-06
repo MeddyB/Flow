7aab6a13a01925050bc3467bd5d5baf4ed238817
meddy <meddy.boukhedouma@gmail.com>
2024-02-06 16:24:09 +0100

    - Feat: Add code for parsing with argsparse module

---
+++ core/core/__main__.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="action")
+    context_parser = subparsers.add_parser("context")
+    context_subparsers = context_parser.add_subparsers(dest="context_action")
+    context_create_parser = context_subparsers.add_parser("create")
+    context_create_parser.add_argument("entity_type", help="Entity Type to create")
+    context_create_parser.add_argument("entity_name", help="Entity Name to create")
+    context_create_parser.add_argument("path")
+    context_create_parser.add_argument("--property", "-p", nargs=2, action="append", help="Entity Properties")
+    context_list_parser = context_subparsers.add_parser("list")
+    context_list_parser.add_argument("path", nargs="?", default=os.getcwd())
+    context_get_parser = context_subparsers.add_parser("get")
+    context_get_parser.add_argument("path")
+    software_parser = subparsers.add_parser("software")
+    software_subparsers = software_parser.add_subparsers(dest="software_action")
+    software_list_parser = software_subparsers.add_parser("list")
+    software_launch_parser = software_subparsers.add_parser("launch")
+    software_launch_parser.add_argument("name", help="Software name to launch")
+    # software_launch_parser.add_argument("--startup-file", "-f", help="Startup Scene")
+    # software_launch_parser.add_argument("--startup-script", "-s", help="Startup Script")
+    action_parser = subparsers.add_parser("action", help="Engine actions")
+    action_subparsers = action_parser.add_subparsers(dest="action_action")
+    action_list = action_subparsers.add_parser("list")
+    action_exec = action_subparsers.add_parser("exec")
+    action_exec.add_argument("name")
+    
+    print(parser.parse_args())
+    
+if __name__ == "__main__":
+    main()
