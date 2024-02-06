620a4046260b17e7b70628fb28bf1725e67f62ba
meddy <meddy.boukhedouma@gmail.com>
2024-02-06 16:41:05 +0100

    - Feat: Add code for argsparse module and         Add code to parsing paths from templates

--- core/core/__main__.py
+++ core/core/__main__.py
@@ -20,15 +20,31 @@ def main():
     software_list_parser = software_subparsers.add_parser("list")
     software_launch_parser = software_subparsers.add_parser("launch")
     software_launch_parser.add_argument("name", help="Software name to launch")
-    # software_launch_parser.add_argument("--startup-file", "-f", help="Startup Scene")
-    # software_launch_parser.add_argument("--startup-script", "-s", help="Startup Script")
     action_parser = subparsers.add_parser("action", help="Engine actions")
     action_subparsers = action_parser.add_subparsers(dest="action_action")
     action_list = action_subparsers.add_parser("list")
     action_exec = action_subparsers.add_parser("exec")
     action_exec.add_argument("name")
     
-    print(parser.parse_args())
+    args = parser.parse_args()
     
+    if args.action == "context":      
+        if args.context_action == "create":
+            print("Creating context {}".format())          
+        elif args.context_action == "list":
+            print("Context list :")
+        elif args.context_action == "get":
+            print("Context get :")
+                    
+    elif args.action == "software": 
+        if args.software_action == "list":
+            print("Softwares list :")
+        elif args.software_action == "launch":
+            print("Exit code is '{}'".format())
+               
+    elif args.action == "action":
+        print("Engine list actions:")
+
+         
 if __name__ == "__main__":
     main()
